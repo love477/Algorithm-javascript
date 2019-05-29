f7758266dc91b025401cfdb20401b8a77f7d9ee4
loveQQ <1296102763@qq.com>
2019-05-29 21:48:49 +0800

    update

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -13,7 +13,6 @@ function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length; j++) {
       if (i === j) continue;
-      if (nums[i] + nums[j] > target) break;
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
