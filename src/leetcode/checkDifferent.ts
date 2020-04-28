9b84efa6014add1db1107dac1dbca8aae503021d
love477 <1296102763@qq.com>
2020-04-28 17:18:52 +0800

    add new algorithm

---
+++ src/leetcode/checkDifferent.ts
@@ -0,0 +1,15 @@
+export function checkDifferent(iniString: string) {
+  if (!iniString) {
+    return false;
+  }
+  for (let index = 0; index < iniString.length; index++) {
+    const regexp = new RegExp('iniString.charAt(index)', 'g');
+    const result = iniString.match(regexp);
+    if (result && result.length > 1) {
+      return false
+    } 
+  }
+  return true;
+}
+
+console.log('result: ', checkDifferent('abcda'));
